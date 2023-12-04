299d78ebeacfab0677edf0ad229c3224340feb10
LEONARDO_DIAS <leonardodias2706@gmail.com>
2023-12-04 17:28:33 -0300

    Kalman step finished

--- src/wKF.py
+++ src/wKF.py
@@ -6,12 +6,13 @@ detector = yl.object_detector()
 cap = cv2.VideoCapture(0)
 
 KF = kalm.KalmanFilter(0.1, 1, 1, 1, 0.1, 0.1)
+x ,y, x1, y1 = 0, 0, 0, 0
 
 while True:
     _, img = cap.read()
 
     center = detector.detect(img)
-
+    
     if center!=None:
         cv2.circle(img, center, radius=20, color=(0, 0, 255), thickness=-1)
 
@@ -22,6 +23,9 @@ while True:
         filtered_center = KF.filter(center)
         x1, y1 = filtered_center[0][0].astype(int), filtered_center[1][0].astype(int)
         cv2.circle(img, (int(x1), int(y1)), radius=20, color=(0, 0, 0), thickness=-1)
+
+    cv2.circle(img, (int(x), int(y)), radius=20, color=(255, 0, 0), thickness=-1)
+    cv2.circle(img, (int(x1), int(y1)), radius=20, color=(0, 0, 0), thickness=-1)
 
     
     cv2.imshow("Detection", img)
