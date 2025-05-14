623dff340ad96ea35bdb11302f64fae6c1530028
LEONARDO_DIAS <leonardodias2706@gmail.com>
2025-05-14 15:40:36 -0300

    cleaning up and update .gitignore

--- src/noKF.py
+++ src/noKF.py
@@ -9,12 +9,12 @@ while True:
 
     center = detector.detect(img)
 
-    if len(center)>0:
+    if len(center) > 0:
         img = cv2.circle(img, center, radius=20, color=(0, 0, 255), thickness=-1)
-    
+
     cv2.imshow("Detection", img)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
